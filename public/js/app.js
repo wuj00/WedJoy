cfdb0886e8c51d0687eb6f40198c28f4375a8903
Jennifer W <w.jennifer123@gmail.com>
2016-04-27 06:37:55 -0700

    added cards---trying to get image to zoom

--- public/js/app.js
+++ public/js/app.js
@@ -1,37 +1,21 @@
 
 
 //PROFILE page -- tab-----
-// function openTab(evt, tabName) {
-//     console.log(onload);
-//     var i, tabcontent, tablinks;
-//     tabcontent = document.getElementsByClassName("tabcontent");
-//     for (i = 0; i < tabcontent.length; i++) {
-//         tabcontent[i].style.display = "none";
-//     }
-//     tablinks = document.getElementsByClassName("tablinks");
-//     for (i = 0; i < tabcontent.length; i++) {
-//         tablinks[i].className = tablinks[i].className.replace(" active", "");
-//     }
-//     document.getElementById(tabName).style.display = "block";
-//     evt.currentTarget.className += " active";
-// }
-
-// jQuery(function($){
-//            $(".tabcontent").children("div").click(function(){
-//                $(".active").removeClass("active");
-//            });
-//        });
-
-jQuery(document).ready(function() {
-    jQuery('.container .tabs a').on('click', function(e)  {
-        var currentAttrValue = jQuery(this).attr('href');
+$(document).ready(function() {
+    $('.container .tabs a').on('click', function(e)  {
+        var currentAttrValue = $(this).attr('href');
 
         // Show/Hide Tabs
-        jQuery('.container ' + currentAttrValue).show().siblings().hide();
+        $('.container ' + currentAttrValue).show().siblings().hide();
 
         // Change/remove current tab to active
-        jQuery(this).parent('li').addClass('active').siblings().removeClass('active');
+        $(this).parent('li').addClass('active').siblings().removeClass('active');
 
         e.preventDefault();
     });
 });
+
+// $(document).ready(function(){
+//     $('.materialboxed').materialbox();
+//
+//   });
